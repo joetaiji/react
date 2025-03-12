90d00a4059b1bd1f3986203a81b06cce102621cb
조정연 <joetaiji@plani.co.kr>
2025-03-12 10:36:39 +0900

    snb적용

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import Gnb from './components/Gnb';
 import App from './components/App';
 import './css/output.css'; // Ensure this path is correct and the file exists
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+	<div id="wrap">
+		<main id="container">
+			<Gnb />
+			<div id="contents" className="contents">
+				<App />
+			</div>
+		</main>
+	</div>
   </React.StrictMode>
 );
